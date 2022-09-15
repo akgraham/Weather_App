bf6884eeebaeaabf29f2f33e48b64cf82bd0f437
akgraham <99699836+akgraham@users.noreply.github.com>
2022-09-15 02:55:42 -0600

    Added Search Engine

--- src/app.js
+++ src/app.js
@@ -40,8 +40,19 @@ function displayTemperature(response) {
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
+function search(city) {
 let apiKey = "0f8bc384a7c31b717a18cfe38a95ae06";
-let city = "New York"
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-
 axios.get(apiUrl).then(displayTemperature);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+search("New York");
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
