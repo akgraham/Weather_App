50807c6bf98eb98e4ca2b62b39340e0026c0b7c9
akgraham <99699836+akgraham@users.noreply.github.com>
2022-09-15 02:26:14 -0600

    Added relevant icon from API

--- src/app.js
+++ src/app.js
@@ -28,12 +28,16 @@ function displayTemperature(response) {
   let humidityElement = document.querySelector("#humidity");
   let speedElement = document.querySelector("#speed");
   let dateElement = document.querySelector("#date");
+  let iconElement = document.querySelector("#icon");
+
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
   speedElement.innerHTML = Math.round(response.data.wind.speed);
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 let apiKey = "0f8bc384a7c31b717a18cfe38a95ae06";
